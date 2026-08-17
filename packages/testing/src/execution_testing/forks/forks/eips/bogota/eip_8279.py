aa15cb42a8f34f94939e76dbaad0158b9e48e972
spencer-tb <spencer.tb@ethereum.org>
2026-08-17 19:53:57 +0200

    feat(tests): activate EIP-8279 at the Bogota pseudo-fork

---
+++ packages/testing/src/execution_testing/forks/forks/eips/bogota/eip_8279.py
@@ -0,0 +1,13 @@
+"""
+EIP-8279: Block Access List Byte Floor.
+
+https://eips.ethereum.org/EIPS/eip-8279
+"""
+
+from ....base_fork import BaseFork
+
+
+class EIP8279(BaseFork):
+    """EIP-8268 class."""
+
+    pass
